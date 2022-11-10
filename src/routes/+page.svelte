e07c096b318ccc1611805f828d53cc3fea5adfa8
Charlie Hutty <charlie.hutty@thespencergroup.co.uk>
2022-11-10 15:48:20 +0000

    SLP-2 Adds card component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,14 @@
-<h1 class="text-4xl underline text-center pt-3">Welcome to SvelteKit</h1>
+<script>
+    import Card from '../lib/Card.svelte';
+</script>
+
+<div class="text-center">
+    <h1 class="text-4xl underline pt-8">Welcome to SvelteKit</h1>
+    <p class="text-2xl pt-4">Hello, world!</p>
+</div>
+
+
+<div class="pt-4">
+    <Card />
+</div>
+
